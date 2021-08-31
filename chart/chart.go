d20b487b8d3b23b7ffe4b01d4348517046cb7466
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2021-08-31 17:27:26 +0200

    chart.go

--- chart/chart.go
+++ chart/chart.go
@@ -7,6 +7,37 @@ type Chart struct {
 	Deps    []Chart
 }
 
-func (c *Chart) Graph() {
+func getChartDependencies(chart Chart) []Chart {
+	return []Chart{
+		{
+			Name:    "1",
+			Repo:    "2",
+			Deps:    nil,
+			Version: "3",
+		},
+		{
+			Name:    "x",
+			Repo:    "y",
+			Deps:    nil,
+			Version: "z",
+		},
+	}
+}
+
+func (chart *Chart) Graph() {
+
+	if chart == nil {
+		return
+	}
+
+	for _, v := range getChartDependencies(*chart) {
+		chart.Deps = append(chart.Deps, v)
+	}
+
+	for _, v := range chart.Deps {
+		if len(v.Deps) > 0 {
+			v.Graph()
+		}
+	}
 
 }
