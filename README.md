2ed4c08bf7443bc90b266225209969e6959857e8
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2021-09-01 12:27:23 +0200

    readme

---
+++ README.md
@@ -0,0 +1,37 @@
+
+
+## Output examples
+
+No dependencies:
+
+```console
+$ helm-graph render --name "common" -r https://charts.bitnami.com/bitnami -v 1.8.0 -o yaml
+name: common
+version: 1.8.0
+repo: https://charts.bitnami.com/bitnami
+deps: []
+```
+
+Flat dependencies:
+
+```console
+$ helm-graph render --name "grafana-tempo" -r https://charts.bitnami.com/bitnami -v 0.2.5 -o yaml
+name: grafana-tempo
+version: 0.2.5
+repo: https://charts.bitnami.com/bitnami
+deps:
+- name: common
+  version: 0.2.5
+  repo: https://charts.bitnami.com/bitnami
+  deps: []
+- name: memcached
+  version: 0.2.5
+  repo: https://charts.bitnami.com/bitnami
+  deps: []
+```
+
+Nested dependencies:
+
+```console
+$ helm-graph render --name "kafka" -r https://charts.bitnami.com/bitnami -v 14.0.5 -o yaml
+```
--- chart/chart.go
+++ chart/chart.go
@@ -1,5 +1,17 @@
 package chart
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/sunnyvale-it/helm-graph/utils"
+	"gopkg.in/yaml.v2"
+)
+
 type Chart struct {
 	Name    string
 	Version string
@@ -8,27 +20,68 @@ type Chart struct {
 }
 
 func getChartDependencies(chart Chart) []Chart {
-	return []Chart{
-		{
-			Name:    "1",
-			Repo:    "2",
-			Deps:    nil,
-			Version: "3",
-		},
-		{
-			Name:    "x",
-			Repo:    "y",
-			Deps:    nil,
-			Version: "z",
-		},
+
+	type dependency struct {
+		Name       string
+		Repository string
+		Version    string
+		Tags       []string
 	}
+
+	tmpDir, err := os.MkdirTemp("", ".helm-graph-tmp-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	archive := fmt.Sprintf("%s-%s.tgz", chart.Name, chart.Version)
+	url := fmt.Sprintf("%s/%s", chart.Repo, archive)
+	filePath := fmt.Sprintf("%s/%s", tmpDir, archive)
+
+	// Get the data
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(fmt.Sprintf("%s", filePath))
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+
+	gzipReader, err := os.Open(filePath)
+
+	os.Mkdir(fmt.Sprintf("%s/%s", tmpDir, chart.Name), 0755)
+
+	err = utils.UntarChartYaml(fmt.Sprintf("%s", tmpDir), gzipReader)
+
+	yfile, err := ioutil.ReadFile(fmt.Sprintf("%s/%s/Chart.lock", tmpDir, chart.Name))
+
+	data := make(map[interface{}]interface{})
+
+	err = yaml.Unmarshal(yfile, &data)
+
+	d, err := yaml.Marshal(data["dependencies"])
+
+	deps := []dependency{}
+
+	err = yaml.Unmarshal([]byte(string(d)), &deps)
+
+	chartDeps := []Chart{}
+
+	for _, v := range deps {
+		tmpChart := &chart
+		tmpChart.Name = v.Name
+
+		chartDeps = append(chartDeps, *tmpChart)
+
+	}
+
+	return chartDeps
+
 }
 
 func (chart *Chart) Graph() {
-
-	if chart == nil {
-		return
-	}
 
 	for _, v := range getChartDependencies(*chart) {
 		chart.Deps = append(chart.Deps, v)
